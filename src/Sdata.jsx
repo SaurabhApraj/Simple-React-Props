c101d771288e783a752c2995c371803042b62900
SaurabhApraj <saurabhapraj7@gmail.com>
2020-07-10 09:28:42 +0530

    Added Sdata file

---
+++ src/Sdata.jsx
@@ -0,0 +1,43 @@
+const Sdata = [
+  {
+    imgsrc:
+      "https://rukminim1.flixcart.com/image/416/416/kae95e80/mobile/2/z/r/motorola-g8-power-lite-pajc0008in-original-imafrz85cmz2ycx8.jpeg?q=70",
+    title: "Motorola G8 Power Lite",
+    price: "₹9,499",
+    link:
+      "https://www.flipkart.com/motorola-g8-power-lite-royal-blue-64-gb/p/itm8a408329ff3d2?pid=MOBFZ5EXNSGJJ2ZR&lid=LSTMOBFZ5EXNSGJJ2ZRBFFMHB&fm=neo%2Fmerchandising&iid=M_22cf4aba-f783-4ac7-bb40-f2841c87a5bb_1.TAWFT0T6QB45&ppt=hp&ppn=homepage&ssid=8h96huttg00000001594346384880&otracker=dynamic_omu_infinite_Best%2BBattery%2BPhones_1_1.dealCard.OMU_INFINITE_Best%2BBattery%2BPhones_TAWFT0T6QB45&cid=TAWFT0T6QB45",
+  },
+  {
+    imgsrc:
+      "https://rukminim1.flixcart.com/image/416/416/kavefm80/mobile/6/y/t/samsung-galaxy-m11-sm-m115fzkeins-original-imafscjyhxwghgwf.jpeg?q=70",
+    title: "Samsung Galaxy M11",
+    price: "₹12,979",
+    link:
+      "https://www.flipkart.com/samsung-galaxy-m11-black-32-gb/p/itm0871b45a83143?pid=MOBFRZZHRFVXDUZZ&lid=LSTMOBFRZZHRFVXDUZZWJ3SQS&fm=neo%2Fmerchandising&iid=M_22cf4aba-f783-4ac7-bb40-f2841c87a5bb_1.9KK7Q86K1XDD&ppt=hp&ppn=homepage&ssid=8h96huttg00000001594346384880&otracker=dynamic_omu_infinite_Best%2BBattery%2BPhones_4_1.dealCard.OMU_INFINITE_Best%2BBattery%2BPhones_9KK7Q86K1XDD&cid=9KK7Q86K1XDD",
+  },
+  {
+    imgsrc:
+      "https://rukminim1.flixcart.com/image/416/416/k9loccw0/mobile/h/f/h/motorola-racer-turbo-edge-xt2061-3-original-imafrcvgpqx23mcq.jpeg?q=70",
+    title: "Motorola Edge+",
+    price: "₹74,999",
+    link:
+      "https://www.flipkart.com/motorola-edge-thunder-grey-256-gb/p/itm89c1f91a0a0e3?pid=MOBFQMAAHRRGMHFH&lid=LSTMOBFQMAAHRRGMHFHMXFIML&fm=neo%2Fmerchandising&iid=M_22cf4aba-f783-4ac7-bb40-f2841c87a5bb_1.D7X0VY507QGY&ppt=hp&ppn=homepage&ssid=8h96huttg00000001594346384880&otracker=dynamic_omu_infinite_Best%2BBattery%2BPhones_5_1.dealCard.OMU_INFINITE_Best%2BBattery%2BPhones_D7X0VY507QGY&cid=D7X0VY507QGY",
+  },
+  {
+    imgsrc:
+      "https://rukminim1.flixcart.com/image/416/416/k6mibgw0/mobile/f/g/v/samsung-galaxy-s20-sm-g980flbdinu-original-imafpfkbfdgfzzgj.jpeg?q=70",
+    title: "Samsung Galaxy S20 ",
+    price: "₹70,499",
+    link:
+      "https://www.flipkart.com/samsung-galaxy-s20-cloud-blue-128-gb/p/itmce5175ce13e7e?pid=MOBFZXZ2RNFPRFGV&lid=LSTMOBFZXZ2RNFPRFGVVHXAH6&fm=neo%2Fmerchandising&iid=M_fc7e11e5-353b-4c9a-8d69-5fa473746d96_1.BU7O8TOLJ7PW&ppt=hp&ppn=homepage&ssid=zj4w8iacmo0000001594352419615&otracker=dynamic_omu_infinite_Best%2BBattery%2BPhones_10_1.dealCard.OMU_INFINITE_Best%2BBattery%2BPhones_BU7O8TOLJ7PW&cid=BU7O8TOLJ7PW",
+  },
+  {
+    imgsrc:
+      "https://rukminim1.flixcart.com/image/416/416/k6fd47k0/mobile/t/a/u/samsung-galaxy-a71-sm-a715fzbwins-original-imafzvtqvz7vtgzs.jpeg?q=70",
+    title: "Samsung Galaxy A71",
+    price: "₹34,499",
+    link:
+      "https://www.flipkart.com/samsung-galaxy-a71-prism-crush-blue-128-gb/p/itmb719fd01880e3?pid=MOBFZ4AJJSWKTTAU&lid=LSTMOBFZ4AJJSWKTTAUXU0B4D&fm=neo%2Fmerchandising&iid=22c781f8-821f-4a4d-8221-7ea26a97f6cf.E5NGQLQ3NAFY&ppt=dynamic&ppn=dynamic&ssid=zj4w8iacmo0000001594352419615&otracker=dynamic_omu_Best%252BBattery%252BPhones_12_Best%252BBattery%252BPhones_E5NGQLQ3NAFY&cid=E5NGQLQ3NAFY",
+  },
+];
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Card from "./Cards";
+import Sdata from "./Sdata";
 
 ReactDOM.render(
   <>
     <Card
-      imgsrc="https://rukminim1.flixcart.com/image/416/416/kae95e80/mobile/2/z/r/motorola-g8-power-lite-pajc0008in-original-imafrz85cmz2ycx8.jpeg?q=70"
-      title="Motorola G8 Power Lite"
-      price="₹9,499"
-      link="https://www.flipkart.com/motorola-g8-power-lite-royal-blue-64-gb/p/itm8a408329ff3d2?pid=MOBFZ5EXNSGJJ2ZR&lid=LSTMOBFZ5EXNSGJJ2ZRBFFMHB&fm=neo%2Fmerchandising&iid=M_22cf4aba-f783-4ac7-bb40-f2841c87a5bb_1.TAWFT0T6QB45&ppt=hp&ppn=homepage&ssid=8h96huttg00000001594346384880&otracker=dynamic_omu_infinite_Best%2BBattery%2BPhones_1_1.dealCard.OMU_INFINITE_Best%2BBattery%2BPhones_TAWFT0T6QB45&cid=TAWFT0T6QB45"
+      imgsrc={Sdata[0].imgsrc}
+      title={Sdata[0].title}
+      price={Sdata[0].price}
+      link={Sdata[0].link}
     />
     <Card
-      imgsrc="https://rukminim1.flixcart.com/image/416/416/kavefm80/mobile/6/y/t/samsung-galaxy-m11-sm-m115fzkeins-original-imafscjyhxwghgwf.jpeg?q=70"
-      title="Samsung Galaxy M11"
-      price="₹12,979"
-      link="https://www.flipkart.com/samsung-galaxy-m11-black-32-gb/p/itm0871b45a83143?pid=MOBFRZZHRFVXDUZZ&lid=LSTMOBFRZZHRFVXDUZZWJ3SQS&fm=neo%2Fmerchandising&iid=M_22cf4aba-f783-4ac7-bb40-f2841c87a5bb_1.9KK7Q86K1XDD&ppt=hp&ppn=homepage&ssid=8h96huttg00000001594346384880&otracker=dynamic_omu_infinite_Best%2BBattery%2BPhones_4_1.dealCard.OMU_INFINITE_Best%2BBattery%2BPhones_9KK7Q86K1XDD&cid=9KK7Q86K1XDD"
+      imgsrc={Sdata[1].imgsrc}
+      title={Sdata[1].title}
+      price={Sdata[1].price}
+      link={Sdata[1].link}
     />
-
     <Card
-      imgsrc="https://rukminim1.flixcart.com/image/416/416/k9loccw0/mobile/h/f/h/motorola-racer-turbo-edge-xt2061-3-original-imafrcvgpqx23mcq.jpeg?q=70"
-      title="Motorola Edge+"
-      price="₹74,999"
-      link="https://www.flipkart.com/motorola-edge-thunder-grey-256-gb/p/itm89c1f91a0a0e3?pid=MOBFQMAAHRRGMHFH&lid=LSTMOBFQMAAHRRGMHFHMXFIML&fm=neo%2Fmerchandising&iid=M_22cf4aba-f783-4ac7-bb40-f2841c87a5bb_1.D7X0VY507QGY&ppt=hp&ppn=homepage&ssid=8h96huttg00000001594346384880&otracker=dynamic_omu_infinite_Best%2BBattery%2BPhones_5_1.dealCard.OMU_INFINITE_Best%2BBattery%2BPhones_D7X0VY507QGY&cid=D7X0VY507QGY"
+      imgsrc={Sdata[2].imgsrc}
+      title={Sdata[2].title}
+      price={Sdata[2].price}
+      link={Sdata[2].link}
+    />
+    <Card
+      imgsrc={Sdata[3].imgsrc}
+      title={Sdata[3].title}
+      price={Sdata[3].price}
+      link={Sdata[3].link}
+    />
+    <Card
+      imgsrc={Sdata[4].imgsrc}
+      title={Sdata[4].title}
+      price={Sdata[4].price}
+      link={Sdata[4].link}
     />
   </>,
   document.getElementById("root")
